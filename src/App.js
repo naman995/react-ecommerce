7e378f1b3336b79372e28f5e68e3ec0d2e1bc45b
naman995 <rajputnaman823@gmail.com>
2023-10-31 00:24:32 +0530

    Login -signup frontend complete and routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import ProductList from "./features/product-list/ProductList";
 import Home from "./pages/Home";
+import Login from "./features/auth/components/Login";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signUp",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
